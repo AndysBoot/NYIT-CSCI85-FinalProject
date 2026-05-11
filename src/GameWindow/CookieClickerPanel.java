6228d748dac5ec238295011328096bb66eb78ccd
Tayebi <maintayebi@gmail.com>
2026-05-11 15:02:59 -0400

    Deleted JFrame in CookieClickerPanel and added addMoney method

--- src/GameWindow/CookieClickerPanel.java
+++ src/GameWindow/CookieClickerPanel.java
@@ -3,56 +3,54 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.Objects;
 
 public class CookieClickerPanel extends JPanel implements ActionListener {
-    private int moneyCount = 1;
+    //Static variables because it applies to the whole class not just one instance of cookieClickerpANEL
+    private static int moneyCount = 0;
 
-    public static void main(String[] args){
-        JFrame frame = new JFrame("Cookie Clicker Panel");
-        frame.setLayout(new BorderLayout());
-
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLocationRelativeTo(null);
-        JPanel cookieClickerPanel = new CookieClickerPanel();
-        frame.add(cookieClickerPanel);
-
-        frame.setVisible(true);
-
-
-    }
-
+    //Default Constructor
     public CookieClickerPanel(){
+        //A layout that centers the button by default
         this.setLayout(new GridBagLayout());
 
+        //Adding an image and setting its size to be 200 x 200
         ImageIcon coinImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("coin.png")));
         Image scaled = coinImg.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+
+        //Creates a button with a image that has no border displayed due to the methods
         JButton cookieBtn = new JButton(new ImageIcon(scaled));
         cookieBtn.setContentAreaFilled(false);
         cookieBtn.setFocusPainted(false);
         cookieBtn.setBorderPainted(false);
 
+        //Listening for a mouseClick
         cookieBtn.addActionListener(this);
-
+        //Add cookie button to the JPanel
         this.add(cookieBtn);
 
     }
 
+    //Get method for moneyCount which can help display the total amount of money
     public int getMoneyCount(){
-        return this.moneyCount;
+        return moneyCount;
     }
 
+    //set method to use in other classes that allows you to add more than 1 dollar
     public void setMoneyCount(int moneyCount){
-        this.moneyCount = moneyCount;
+        CookieClickerPanel.moneyCount = moneyCount;
+    }
+    //Method for other classes to add a custom amount of money or take away
+    public void addMoney(int amount) {
+        CookieClickerPanel.moneyCount += amount;
     }
 
-
+    //Method that listens for a mouse click and increments the total money count by 1
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Clicked");
-        System.out.println();
+        // System.out.println("Clicked");
+        moneyCount = moneyCount + 1;
+       // System.out.println(getMoneyCount());
+
     }
 }
--- src/GameWindow/GameTimer.java
+++ src/GameWindow/GameTimer.java
@@ -38,4 +38,6 @@ public class GameTimer {
     public Instant getCurrentTime(){
         return this.gameCurrentTime;
     }
+
+
 }
