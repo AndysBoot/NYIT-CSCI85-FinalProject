fcacd019765e20112da930674e904285f441c54b
andys <andysosa2007@gmail.com>
2026-05-11 14:00:54 -0400

    Saving Progress

--- src/GameWindow/GameTimer.java
+++ src/GameWindow/GameTimer.java
@@ -1,4 +1,40 @@
 package GameWindow;
 
 public class GameTimer {
+    protected Instant gameStartTime;
+    protected Instant gameCurrentTime;
+    public GameTimer() {
+
+
+        Instant gameStartTime = Instant.now();
+        int i = 0;
+        Instant end = null;
+        try {
+        while (i < 3) {
+            Instant start = Instant.now();
+            Thread.sleep(15000);
+            end = Instant.now();
+            System.out.println("Amount of seconds passed" + Duration.between(start, end).toSeconds());
+            i++;
+        }
+    }
+        catch(InterruptedException e){
+        e.printStackTrace();
+    }
+        catch(Exception e){
+        //this is here in case there's an error we don't know about
+        e.printStackTrace();
+    }
+
+    long secondsElapsed = Duration.between(gameStartTime, end).toSeconds();
+        System.out.println("secondsElapsed = "+secondsElapsed);
+    }
+
+    public Instant getStartTime(){
+        return this.gameStartTime;
+    }
+
+    public Instant getCurrentTime(){
+        return this.gameCurrentTime;
+    }
 }
